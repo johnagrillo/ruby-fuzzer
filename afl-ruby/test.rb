98acfd6a6451ebb2d8ba76fb2e5af26eed8cb250
johnagrillo@yahoo.com <johnagrillo>
2023-05-16 17:54:36 -0400

    updae

--- afl-ruby/test.rb
+++ afl-ruby/test.rb
@@ -1,6 +1,8 @@
 require_relative('lib/afl')
 
 
+input = FIle.open(ARGV[0])
+
 
 def function1
   return 1
@@ -10,16 +12,15 @@ def function2
   return 2
 end
 
-AFL.init
-AFL.with_exceptions_as_crashes do
-  input = stdin.read(1)
-  if input == '7'
-    raise 'I hate the number 7'
-  elsif input.ord % 2 == 0
-    function1
-  else
-    function2
-  end
-  exit!(0)
+input = stdin.read(1)
+puts input
+if input == '7'
+  throw  'I hate the number 7'
+elsif input.ord % 2 == 0
+  function1
+else
+  function2
 end
+exit!(0)
+
 
