58eae0b0f22f09b82693b5b07b9f0ec5c04759e5
johnagrillo@yahoo.com <johnagrillo>
2023-05-16 18:08:20 -0400

    updae

--- test.rb
+++ test.rb
@@ -1,3 +1,4 @@
+$stderr.puts ARGV[0]
 input = File.open(ARGV[0])
 def function1
   return 1
@@ -7,8 +8,8 @@ def function2
   return 2
 end
 
-input = stdin.read(1)
-puts input
+input = input.read(1)
+$stderr.puts input
 if input == '7'
   throw  'I hate the number 7'
 elsif input.ord % 2 == 0
